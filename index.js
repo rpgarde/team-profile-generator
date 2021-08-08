a86bc81236bebe5a02be96ed260ac50899440b7f
rpgarde <rpgarde@gmail.com>
2021-08-08 11:32:23 +1000

    Added all prompts

--- index.js
+++ index.js
@@ -1,11 +1,108 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
 const Employee = require('./lib/Employee.js')
 const Manager = require('./lib/Manager.js')
 const Intern = require('./lib/Intern.js')
 const Engineer = require('./lib/Engineer.js')
 
+// const empl = new Employee('Paolo',10,'rpgarde@gmail.com')
+// const mgr = new Manager('Paolo Garde',10,'rpgarde@gmail.com',4)
+const emplArr = [] 
 
-const empl = new Employee('Paolo',10,'rpgarde@gmail.com')
-const mgr = new Manager('Paolo Garde',10,'rpgarde@gmail.com',4)
+const allQuestions = [{
+    type: "input",
+    message: "Please enter name",
+    name: "name"
+  }, {
+    type: "input",
+    message: "Please enter ID",
+    name: "id"
+  },
+  {
+    type: "input",
+    message: "Please enter Email",
+    name: "email"
+  }
+];
 
-console.log(empl.getName())
-console.log(mgr)
+const internQuestion = {
+    type: "input",
+    message: "Please enter intern's school",
+    name: "school"
+  }
+
+const managerQuestion = {
+type: "number",
+message: "Please enter manager's office number",
+name: "officeNumber"
+}
+
+const engineerQuestion = {
+    type: "input",
+    message: "Please enter engineer's GitHub username",
+    name: "github"
+  }
+
+const addChoice = [{
+    type: "list",
+    message: "What type of team member would you like to add?",
+    name: "role",
+    choices: [
+      "Engineer",
+      "Intern",
+      "No more employees to add"
+    ]
+  }];
+
+// function for manager (put into init )
+
+const employeeChoice = () => {
+    inquirer.prompt(addChoice)
+    .then((data)=>
+    {
+        if(data.role === "Engineer"){
+            engineerPrompt()
+        }
+        else if(data.role === "Intern"){
+            internPrompt()
+        }
+        else{
+            console.log("All employees added!")
+            // write file function here
+        }
+    })
+}
+
+const init = () => {
+    inquirer.prompt([...allQuestions,managerQuestion])
+    .then((data)=>{
+    let manager = new Manager(data)
+    emplArr.push(data)
+    console.log(emplArr)
+    employeeChoice()
+    console.log('successful')
+    });
+}
+
+const engineerPrompt = () => {
+    inquirer.prompt([...allQuestions,engineerQuestion])
+    .then((data)=>{
+    let engineer = new Engineer(data)
+    emplArr.push(data)
+    console.log(emplArr)
+    employeeChoice()
+    });
+}
+
+const internPrompt = () => {
+    inquirer.prompt([...allQuestions,internQuestion])
+    .then((data)=>{
+    let intern= new Intern(data)
+    emplArr.push(data)
+    console.log(emplArr)
+    employeeChoice()
+    });
+}
+
+
+init()
