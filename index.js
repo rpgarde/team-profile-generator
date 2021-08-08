48b173f7a2044e024d3885ea8b6e5fdeba673e8d
rpgarde <rpgarde@gmail.com>
2021-08-08 14:06:23 +1000

    added licence, added generated template

--- index.js
+++ index.js
@@ -6,118 +6,126 @@ const Intern = require('./lib/Intern.js')
 const Engineer = require('./lib/Engineer.js')
 const generateHTML = require('./lib/generateHTML.js')
 
-// const empl = new Employee('Paolo',10,'rpgarde@gmail.com')
-// const mgr = new Manager('Paolo Garde',10,'rpgarde@gmail.com',4)
-const emplArr = [] 
+// create empty array to add responses to
+const emplArr = []
+
+const validator = (input) => {
+    if (input.length === 0) {
+        return 'Please provide an input';
+    }
+    else {
+        return true;
+    }
+}
 
 const allQuestions = [{
     type: "input",
     message: "Please enter name",
-    name: "name"
-  }, {
+    name: "name",
+    validate: validator
+}, {
     type: "input",
     message: "Please enter ID",
-    name: "id"
-  },
-  {
+    name: "id",
+    validate: validator
+},
+{
     type: "input",
     message: "Please enter Email",
-    name: "email"
-  }
+    name: "email",
+    validate: validator
+}
 ];
 
 const internQuestion = {
     type: "input",
     message: "Please enter intern's school",
-    name: "school"
-  }
+    name: "school",
+    validate: validator
+}
 
 const managerQuestion = {
-type: "number",
-message: "Please enter manager's office number",
-name: "officeNumber"
+    type: "number",
+    message: "Please enter manager's office number",
+    name: "officeNumber",
+    validate: validator
 }
 
 const engineerQuestion = {
     type: "input",
     message: "Please enter engineer's GitHub username",
-    name: "github"
-  }
+    name: "github",
+    validate: validator
+}
 
 const addChoice = [{
     type: "list",
     message: "What type of team member would you like to add?",
     name: "role",
     choices: [
-      "Engineer",
-      "Intern",
-      "No more employees to add"
+        "Engineer",
+        "Intern",
+        "No more employees to add"
     ]
-  }];
+}];
 
-// function for manager (put into init )
-
-const employeeChoice = () => {
-    inquirer.prompt(addChoice)
-    .then((data)=>
-    {
-        if(data.role === "Engineer"){
-            engineerPrompt()
-        }
-        else if(data.role === "Intern"){
-            internPrompt()
-        }
-        else{
-            console.log("All employees added!")
-            writeToFile("team.html",generateHTML(emplArr))
-        }
-    })
-}
-
-const init = () => {
-    inquirer.prompt([...allQuestions,managerQuestion])
-    .then((data)=>{
-    let manager = new Manager(data.name, data.id, data.email, data.officeNumber)
-    manager.role = manager.getRole()
-    console.log(manager.name)
-    emplArr.push(manager)
-    console.log(emplArr)
-    employeeChoice()
-    });
-}
-
-const engineerPrompt = () => {
-    inquirer.prompt([...allQuestions,engineerQuestion])
-    .then((data)=>{
-    let engineer = new Engineer(data.name, data.id, data.email, data.github)
-    engineer.role = engineer.getRole()
-    emplArr.push(engineer)
-    console.log(emplArr)
-    employeeChoice()
-    });
-}
-
-const internPrompt = () => {
-    inquirer.prompt([...allQuestions,internQuestion])
-    .then((data)=>{
-    let intern= new Intern(data.name, data.id, data.email, data.school)
-    intern.role = intern.getRole()
-    emplArr.push(intern)
-    console.log(emplArr)
-    employeeChoice()
-    });
-}
-
-// Write function
+// write file function
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
         if (err) {
-          throw err;
+            throw err;
         }
-        console.log("File created!");
-      });
+        console.log("File created! Please check team.html\n");
+    });
 }
 
+// function which asks next employee to add (or option to finish process)
+const employeeChoice = () => {
+    inquirer.prompt(addChoice)
+        .then((data) => {
+            if (data.role === "Engineer") {
+                engineerPrompt()
+            }
+            else if (data.role === "Intern") {
+                internPrompt()
+            }
+            else {
+                writeToFile("team.html", generateHTML(emplArr))
+            }
+        })
+}
 
+const init = () => {
+    inquirer.prompt([...allQuestions, managerQuestion])
+        .then((data) => {
+            let manager = new Manager(data.name, data.id, data.email, data.officeNumber)
+            manager.role = manager.getRole()
+            emplArr.push(manager)
+            console.log(`Added ${manager} to the list of employees\n`)
+            employeeChoice()
+        });
+}
+
+const engineerPrompt = () => {
+    inquirer.prompt([...allQuestions, engineerQuestion])
+        .then((data) => {
+            let engineer = new Engineer(data.name, data.id, data.email, data.github)
+            engineer.role = engineer.getRole()
+            emplArr.push(engineer)
+            console.log(`Added ${engineer} to the list of employees\n`)
+            employeeChoice()
+        });
+}
+
+const internPrompt = () => {
+    inquirer.prompt([...allQuestions, internQuestion])
+        .then((data) => {
+            let intern = new Intern(data.name, data.id, data.email, data.school)
+            intern.role = intern.getRole()
+            emplArr.push(intern)
+            console.log(`Added ${intern} to the list of employees\n`)
+            employeeChoice()
+        });
+}
 
 init()
